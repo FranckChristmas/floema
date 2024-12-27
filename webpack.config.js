870a3f8536f0f31a22ab5ec906028adac7c2d51f
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2024-12-27 19:27:01 +0100

    still config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,65 @@
 const path = require('path');
 const webpack = require('webpack');
-
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const IS_DEV = process.env.NODE_ENV === 'development';
 const dirApp = path.join(__dirname, 'app');
 const dirAssets = path.join(__dirname, 'assets');
-const dirStyles = path.join(__dirname, 'styles ');
+const dirStyles = path.join(__dirname, 'styles');
+
+module.exports = {
+  entry: {
+    app: dirApp
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEV: JSON.stringify(IS_DEV)
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: dirAssets, to: 'assets' }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp4)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
