0ffb553bfe8a7ef97ebfdadccadf0e964ae7aa7a
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-02-06 00:31:16 +0100

    installing prismic

--- app.js
+++ app.js
@@ -8,6 +8,33 @@ const app = express()
 const path = require('path')
 const port = 3000
 
+const Prismic = require('@prismicio/client')
+var PrismicDOM = require('prismic-dom')
+
+const initApi = (req) => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    req
+  })
+}
+
+const handleLinkResolver = doc => { // Define the URL depending on the document type
+//   if (doc.type === 'product') {
+//     return `/detail/${doc.id}`
+//   } else if (doc.type === 'collection') {
+//     return `/collections/${doc.id}`
+//   }
+  return '/'
+}
+
+app.use((req, res, next) => {
+res.locals.ctx = {
+  endpoint: process.env.PRISMIC_ENDPOINT,
+  linkResolver: handleLinkResolver
+}
+res.locals.PrismicDOM = PrismicDOM
+next()
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
