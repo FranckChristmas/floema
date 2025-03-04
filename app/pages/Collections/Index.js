f198a729a57eb86fcd51f0b06f7a6d4a4f7f93e0
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-03-04 11:25:28 +0100

    adding selector for each pages

--- app/pages/Collections/Index.js
+++ app/pages/Collections/Index.js
@@ -3,7 +3,8 @@ import Page from "classes/Page";
 export default class Collections extends Page {
     constructor () {
       super({ 
-        id: 'collections'
+        id: 'collections',
+        element: '.collections'
     })
   }
-}
+} 
