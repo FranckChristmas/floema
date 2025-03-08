ba702b46009f348450aaaa73ddcfbaa0aa3327a5
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-03-09 00:37:31 +0100

    imeplementation of the home picture webgl grid

--- app/components/Canvas/Index.js
+++ app/components/Canvas/Index.js
@@ -1,6 +1,6 @@
-import { Box, Camera, Renderer, Program, Mesh, Transform, } from 'ogl'  
-import vertex from 'shaders/plane-vertex.glsl'
-import fragment from 'shaders/plane-fragment.glsl'
+import { Camera, Renderer, Transform } from 'ogl'  
+
+import Home from 'components/Home'
 // Camera, Renderer and Transform are the three elements needed to create a 3D scene -
 // Box, Program and Mesh are the three elements needed to create a 3D object
 // all the elements are imported from the ogl library
@@ -11,7 +11,7 @@ export default class Canvas {
     this.createCamera()
     this.createRenderer()
     this.createScene()
-    this.createCube()
+    this.createHome()
 
   }
 
@@ -29,22 +29,13 @@ export default class Canvas {
   }
 
   createScene() {
-    this.scene = new Transform
+    this.scene = new Transform()
   }
-  createCube() {
-    this.geometry = new Box(this.gl)
-
-  this.program = new Program(this.gl, { // create a new program with the following vertex and fragment shaders
-      vertex,
-      fragment,
-  });
-
-  this.mesh = new Mesh(this.gl, { // create a new mesh with the following parameters
-    geometry : this.geometry,
-    program : this.program
-  }) // create a new mesh with the geometry and the program
-
-  this.mesh.setParent(this.scene) // set the parent of the mesh to the scene
+  createHome() {
+    this.home = new Home({
+      gl: this.gl,
+      scene: this.scene
+    })
   }
 
   onResize() {
@@ -55,12 +46,9 @@ export default class Canvas {
     });
   }
   update() {
-    this.mesh.rotation.y -= 0.01 // rotate the mesh on the y-axis
-    this.mesh.rotation.x -= 0.01 // rotate the mesh on the x-axis
-
     this.renderer.render({
+      camera: this.camera,
       scene: this.scene,
-      camera: this.camera
     })
   }
 }
