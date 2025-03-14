f235321997428b303a3e312e4095bbb5abf157ac
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-03-14 02:06:41 +0100

    deleting the flooding of animation of all the text stuff

--- app/animations/Highlight.js
+++ app/animations/Highlight.js
@@ -1,10 +1,6 @@
 import GSAP from 'gsap'
 import Animation from '../classes/Animation'
 
-import { calculate, split } from '../utils/text'
-import each from 'lodash/each'
-
-
 export default class Highlight extends Animation {
   constructor ({ element, elements }) {
     super({
@@ -22,22 +18,14 @@ export default class Highlight extends Animation {
 
     this.timelineIn.fromTo(this.element, {
       autoAlpha: 0,
-      scale: 1.2,
     }, {
        autoAlpha: 1,
-       duration: 1.5,
-       ease: 'expo.out',
-        scale: 1,
-
+       duration: 1,
       });
   }
   animateOut() {
     GSAP.set(this.element, {
       autoAlpha: 0
     })
-  }
-
-  onResize() {
-
   }
 }
